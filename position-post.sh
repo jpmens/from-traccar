2c004fa67bd258c51e99d3d3732adc553f59d1ed
Jan-Piet Mens <jp@mens.de>
2024-02-18 13:21:47 +0100

    tankensub becomes historic 	addresses https://github.com/jpmens/owntracks-de-ansible/issues/67

--- position-post.sh
+++ position-post.sh
@@ -1,6 +1,6 @@
 curl -v \
 	--header "Content-Type: application/json" \
-	--data @p.json \
+	--data @pos-without-lat-lon.json \
 	http://127.0.0.1:8840/evpos
 
-netstat -an|grep -c "8840.*ESTABLISHED"
+# netstat -an|grep -c "8840.*ESTABLISHED"
